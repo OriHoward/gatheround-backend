b1e5363156dbc51969d42d6e1e5ccdcfb2d71569
OriHoward <ori.howard@gmail.com>
2022-12-08 19:34:44 +0200

    resolved conflict

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from Resources.User import User
 from Resources.Login import Login
 from Resources.Refresh import Refresh
 from Resources.Invite import Invite
+from Resources.Business import Business
 from server import api, app, db
 
 # This initializes the routers
@@ -11,8 +12,9 @@ api.add_resource(User, '/users')
 api.add_resource(Login, '/login')
 api.add_resource(Refresh, '/refresh')
 api.add_resource(Invite, '/invite')
+api.add_resource(Business, '/business')
 
 if __name__ == '__main__':
-    db.drop_all()
+    # db.drop_all()
     db.create_all()
     app.run(debug=True)
