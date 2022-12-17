70ff7ada2040230ebf97a530fadd8f06e862e73c
OriHoward <ori.howard@gmail.com>
2022-12-18 00:42:10 +0200

    Updated get request in Business.py to get all user_business details

--- Resources/Business.py
+++ Resources/Business.py
@@ -2,6 +2,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 from flask_restful import Resource
 from flask import request
 from Models.BusinessRecord import BusinessRecord
+from Models.UserRecord import UserRecord
 from server import db
 
 
@@ -9,8 +10,10 @@ class Business(Resource):
     @jwt_required()
     def get(self):
         curr_user = get_jwt_identity()
-        business_info: BusinessRecord = BusinessRecord.query.filter_by(id=curr_user.get("id")).first()
-        return business_info.serialize()
+        user_id = curr_user.get("id")
+        user_record: UserRecord = UserRecord.query.filter_by(id=user_id).first()
+        business_record: BusinessRecord = BusinessRecord.query.filter_by(id=user_record.id).first()
+        return {"userRecord": user_record.serialize(), "businessRecord": business_record.serialize()}
 
     def post(self):
         received_data = request.json
