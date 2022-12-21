cba79b13415ef870c01689ccbc9e67adaa853f0a
OriHoward <ori.howard@gmail.com>
2022-12-21 19:16:17 +0200

    Added get request for BusinessPackage

--- Resources/BusinessPackage.py
+++ Resources/BusinessPackage.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import request
 from server import db
 from Models.BusinessPackageRecord import BusinessPackageRecord
@@ -9,18 +9,31 @@ class BusinessPackage(Resource):
 
     @jwt_required()
     def get(self):
-        business_id = get_jwt_identity().get("id")
+        """
+        The get request returns the Packages created by the business user
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument('package-limit', location='args')
+        args = parser.parse_args()
+        all_packages: BusinessPackageRecord = db.session.query(BusinessPackageRecord). \
+            order_by(BusinessPackageRecord.price.desc()).limit(args.get("package-limit")).all()
+        my_packages = list(map(lambda entry: entry.serialize(), all_packages))
+        return {"my_packages": my_packages}
+
         pass
 
     @jwt_required()
     def post(self):
+        """
+        The post request inserts the data into the Package table
+        """
         received_data = request.json
         business_id = get_jwt_identity().get("id")
         package_name = received_data.get("packageName")
         description = received_data.get("description")
         currency = received_data.get("currency")
         price = received_data.get("price")
-        curr_package: BusinessPackageRecord = BusinessPackageRecord(id=business_id, package_name=package_name,
+        curr_package: BusinessPackageRecord = BusinessPackageRecord(user_id=business_id, package_name=package_name,
                                                                     description=description, currency=currency,
                                                                     price=price)
 
