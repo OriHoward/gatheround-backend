a3b4d1ce1b4ee352eefc0b55ded495c6f4070339
Eitan Kats <eitan.kats@sightfull.com>
2023-01-07 18:56:17 +0200

    added error to handle double booking

--- Resources/RequestRouter.py
+++ Resources/RequestRouter.py
@@ -3,6 +3,8 @@ from Models.RequestRecord import RequestRecord
 from flask import request
 from flask_jwt_extended import jwt_required
 from server import db
+from flask import abort
+
 
 
 class RequestRouter(Resource):
@@ -13,8 +15,13 @@ class RequestRouter(Resource):
         event_id = received_data.get("desiredEventId")
         package_id = received_data.get("packageId")
         if not business_id or not event_id or not package_id:
-            return 406
+            abort(406)
 
+        existing_record = RequestRecord.query.filter(RequestRecord.event_id == event_id,
+                                                     RequestRecord.package_id == package_id).all()
+        if len(existing_record) > 0:
+            abort(409)
+        print(existing_record)
         request_record: RequestRecord = RequestRecord(package_id=package_id,
                                                       event_id=event_id,
                                                       description=received_data.get("description", "")
