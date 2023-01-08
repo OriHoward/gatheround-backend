14d957551433d7cacef404d8f7f72cd2535e4f5b
OriHoward <ori.howard@gmail.com>
2023-01-08 15:53:12 +0200

    Increased description length to 1000

--- Models/BusinessPackageRecord.py
+++ Models/BusinessPackageRecord.py
@@ -6,7 +6,7 @@ class BusinessPackageRecord(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     user_id = db.Column(db.Integer, ForeignKey("user_record.id"))
     package_name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(200))
+    description = db.Column(db.String(1000))
     price = db.Column(db.Integer, nullable=False)
     currency = db.Column(db.String(3), nullable=False)
 
