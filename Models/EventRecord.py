9b3ab3a929b41cef2eaf74a439493641c00d53eb
OriHoward <ori.howard@gmail.com>
2023-01-08 15:53:42 +0200

    Added category column to table

--- Models/EventRecord.py
+++ Models/EventRecord.py
@@ -5,6 +5,7 @@ class EventRecord(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     event_date = db.Column(db.DateTime)
+    category = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(100))
     description = db.Column(db.String(200))
     limit_attending = db.Column(db.Integer, default=100)
@@ -14,6 +15,7 @@ class EventRecord(db.Model):
             "id": self.id,
             "name": self.name,
             "event_date": self.event_date.strftime('%d/%m/%Y %H:%M'),
+            "category" : self.category,
             "address": self.address,
             "description": self.description,
             "limit_attending": self.limit_attending
